55204d0c3be7a13bbf26c6fb1e57b3871355aa37
KSKNico <nifrabe@outlook.de>
2024-07-02 17:10:58 +0200

    rename file

---
+++ fingerTable.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <array>
+#include "hash.hpp"
+#include "Poco/Net/SocketStream.h"
+
+class FingerTable {
+    public:
+        /* void addFinger();
+        void removeFinger();
+        void updateFinger();
+        void getFinger();*/
+        constexpr static int FINGER_TABLE_SIZE = Hash::HASH_EXPONENT;
+    private:
+        std::array<Hash, FINGER_TABLE_SIZE> fingers;
+        Hash predecessor;
+        Hash successor;
+};
