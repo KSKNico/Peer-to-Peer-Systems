079370edc7436daf6c66f6fcd2119324826b243b
TheT87 <tobikoellner@gmail.com>
2024-07-05 00:00:13 +0200

    first async primes

--- peerConfig.cpp
+++ peerConfig.cpp
@@ -5,7 +5,7 @@ PeerConfig::PeerConfig(std::string filename) {
     loadConfig("peerConfig.json");
 }
 
-void PeerConfig::loadConfig(std::string filename) {
+void PeerConfig::loadConfig(const std::string& filename) {
     std::ifstream file(filename);
     nlohmann::json data = nlohmann::json::parse(file);
     file.close();
@@ -25,8 +25,8 @@ std::vector<sockaddr_in> PeerConfig::getKnownHosts() const {
     return knownHosts;
 }
 
-sockaddr_in PeerConfig::convertToAddress(std::string addressString) {
-    struct sockaddr_in address;
+sockaddr_in PeerConfig::convertToAddress(const std::string& addressString) {
+    struct sockaddr_in address{};
     address.sin_family = AF_INET;
     // the addressString has the format x.x.x.x:yyyy
     auto colon = addressString.find(':');
@@ -36,3 +36,4 @@ sockaddr_in PeerConfig::convertToAddress(std::string addressString) {
     inet_pton(AF_INET, ip.c_str(), &address.sin_addr);
     return address;
 }
+
