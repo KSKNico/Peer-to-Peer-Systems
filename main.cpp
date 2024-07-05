25a7825333d19142542b3d0ed6480be2422cbc06
TheT87 <tobikoellner@gmail.com>
2024-07-05 15:50:36 +0200

    future fixed

--- main.cpp
+++ main.cpp
@@ -9,7 +9,14 @@
 
 
 int main() {
-    ///*
+
+    std::future<vector<unsigned long long >> testFuture =  primeCalculation::calculateCurrentSector();
+    vector<unsigned long long> result = testFuture.get();
+    /*
+    for (auto v : result) {
+        cout << v << " ";
+    }
+    */
     auto peer_addr_1 = Poco::Net::SocketAddress("127.0.0.1:5001");
     auto peer_addr_2 = Poco::Net::SocketAddress("127.0.0.1:5002");
     auto peer_addr_3 = Poco::Net::SocketAddress("127.0.0.1:5003");
@@ -17,9 +24,8 @@ int main() {
     Peer peer_1(peer_addr_1);
     Peer peer_3(peer_addr_3);
     Peer peer_2(peer_addr_2, {peer_addr_1, peer_addr_3});
-    //*/
 
-    primeCalculation::calculateCurrentSector();
+
 
     while(1) {}
 }
