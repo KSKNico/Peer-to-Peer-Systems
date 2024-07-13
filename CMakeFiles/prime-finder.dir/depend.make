e0b5b499c84813a4c450759040b4ae66d802ad5a
leonie <hoernchen772@web.de>
2024-07-13 23:50:09 +0200

    find own ip addresses

---
+++ CMakeFiles/prime-finder.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for prime-finder.
+# This may be replaced when dependencies are built.
---
+++ tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/leonie/Dokumente/Master/P2P/Peer-to-Peer-Systems/tests
+# Build directory: /home/leonie/Dokumente/Master/P2P/Peer-to-Peer-Systems/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/leonie/Dokumente/Master/P2P/Peer-to-Peer-Systems/tests/runTests[1]_include.cmake")
+subdirs("../_deps/googletest-build")
