ffb8c4f66d5aa37592fac627995f021139a50b28
KSKNico <nifrabe@outlook.de>
2024-09-17 17:08:32 +0200

    template function moved to header

--- src/timing.cpp
+++ src/timing.cpp
@@ -13,13 +13,3 @@ std::chrono::steady_clock::time_point Timing::getStarted() const {
 std::chrono::steady_clock::time_point Timing::getLastUpdated() const {
     return lastUpdated;
 }
-
-template <
-    class result_t   = std::chrono::milliseconds,
-    class clock_t    = std::chrono::steady_clock,
-    class duration_t = std::chrono::milliseconds
->
-static auto since(std::chrono::time_point<clock_t, duration_t> const& start)
-    {
-        return std::chrono::duration_cast<result_t>(clock_t::now() - start);
-    }
--- src/timing.hpp
+++ src/timing.hpp
@@ -17,8 +17,7 @@ class Timing {
         class clock_t    = std::chrono::steady_clock,
         class duration_t = std::chrono::milliseconds
     >
-    static auto since(std::chrono::time_point<clock_t, duration_t> const& start)
-    {
+    static auto since(std::chrono::time_point<clock_t, duration_t> const& start) {
         return std::chrono::duration_cast<result_t>(clock_t::now() - start);
     }
 
