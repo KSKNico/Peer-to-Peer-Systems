1c8de8653124ad895a5e5bef0f0335ef1a0e47a0
KSKNico <nifrabe@outlook.de>
2025-02-09 19:54:50 +0100

    join test works now for two peers. Added some hard coded functionality for the bootstrapping peer.

--- src/fingerTable.cpp
+++ src/fingerTable.cpp
@@ -1,6 +1,7 @@
 #include "fingerTable.hpp"
 
 #include <cassert>
+#include "spdlog/spdlog.h"
 
 FingerTable::FingerTable(const Poco::Net::SocketAddress &ownAddress) : ownAddress(ownAddress),
                                                                        ownHash(Hash(ownAddress)),
@@ -40,9 +41,17 @@ Poco::Net::SocketAddress FingerTable::getSuccessor() const {
 }
 
 void FingerTable::setSuccessor(const Poco::Net::SocketAddress &successor) {
+    spdlog::get(this->ownAddress.toString())->debug("Setting successor to {}", successor.toString());
+
     if (this->successor == ownAddress) {
         // this means that the peer is the only peer in the network
         this->successor = successor;
+        
+        // if the predecessor is the peer itself then the predecessor must be the new successor
+        if (this->predecessor == ownAddress) {
+            this->predecessor = successor;
+        }
+        return;
     }
 
     if (Hash(successor).isBetween(ownHash, Hash(this->successor))) {
@@ -51,6 +60,13 @@ void FingerTable::setSuccessor(const Poco::Net::SocketAddress &successor) {
 }
 
 void FingerTable::setPredecessor(const Poco::Net::SocketAddress &predecessor) {
+    // if there is no successor and no predecessor, set the predecessor to the new predecessor
+    if (this->successor == ownAddress && this->predecessor == ownAddress) {
+        this->predecessor = predecessor;
+        this->successor = predecessor;
+        return;
+    }
+
     // if the current predecessor is not set, set it to the new predecessor
     if (this->predecessor == ownAddress) {
         assert(Hash(ownAddress).isBetween(Hash(predecessor), Hash(this->successor)));
@@ -69,7 +85,7 @@ Poco::Net::SocketAddress FingerTable::getPredecessor() const {
 }
 
 void FingerTable::updateWithAddress(const Poco::Net::SocketAddress &address) {
-    assert(address != ownAddress);
+    // assert(address != ownAddress);
     Hash addressHash(address);
     for (unsigned int i = 0; i < FINGER_TABLE_SIZE; i++) {
         auto fingerHash = Hash::fromExponent(i) + ownHash;
